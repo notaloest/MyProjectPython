554cdff4929596b51bebdfe3ea930851f9a53649
notaloest <ILKML@ya.ru>
2022-12-14 13:29:17 +0300

    Initial commit

---
+++ Progress.py
@@ -0,0 +1,109 @@
+# 1.1
+# name = input('What is your name?')
+# age = input('How old are you?')
+# counry = input('where are you live')
+# print(name, " ", age, " ", counry)
+
+# 1.2
+# example = input('Решите пример 4*100-54:1')
+# answer = 4 * 100 - 54
+# print(answer)
+
+# 1.3
+# a = int(input('A: '))
+# b = int(input('B: '))
+# c = int(input('C: '))
+# d = int(input('D: '))
+# ab = a + b
+# cd = c + d
+# print((ab) / (cd))
+
+# 1.4
+# num = (4 + 5 * 60 - 17 + (65 / 4))
+# sart = int(num ** 1 / 2)
+# print(sart)
+
+# ПР2:
+
+# Задание 1
+# a=float(input('1-ое число'))
+# b=float(input('2-е число'))
+# if a>b:
+# print('True')
+# else:
+# print('False')
+# Задание 2
+# a=float(input('Введи число'))
+# if a>=1:
+# print(1)
+# elif a<= -1:
+# print(-1)
+# else:
+# print(0)
+# Задание 3
+# a=float(input('Введи 1-е чило'))
+# b=float(input('Введи 2-е чило'))
+# c=float(input('Введи 3-е чило'))
+# sum_pol=float(0)
+# kol_otr=int(0)
+# sum_otr=float(0)
+# if a >= 0:
+# sum_pol = a
+# else:
+# sum_otr = a
+# kol_otr = 1
+# if b >= 0:
+# sum_pol = sum_pol+b
+# else:
+# sum_otr = sum_otr+b
+# kol_otr = kol_otr+1
+# if c >= 0:
+# sum_pol = sum_pol+c
+# else:
+# sum_otr = sum_otr+c
+# kol_otr = kol_otr+1
+# print('Ссума положительных = {0}'.format(sum_pol),'Ссума отрицательных = {0}'.format(sum_otr),'Колличество отрицательных = {0}'.format(kol_otr),sep='\n')
+
+# Задание 4
+# t = int(input('Введи время в минутах от 1 до 59'))
+# if t%5<=3:
+# print('зелёный')
+# elif t%5 == 4:
+# print('красный')
+# else:
+# print('красный')
+
+# Задание 5
+# 1
+# mass=float(input('Привет! Введи вес для расчёта (в килограммах)'))
+# height=float(input('А и про рост не забудь (обязательно в метрах)'))
+# imt=mass/(height**2)
+# if imt>=24.99:
+# print('худеть')
+# elif imt<=18.5:
+# print('набрать вес')
+# else:
+# print('Ты прекрасен')
+# 2
+# balance=float(input('Сколько мани на кормане?'))
+#
+# if balance>=5000:
+# print('Бро сегодня шикуем! Ресторан жди нас')
+# elif balance<=2500:
+# print('Ты нищий жри картон')
+# else:
+# print('Ну чем богаты. Топай в KFC')
+# 3
+# num=int(input('Введи целое число'))
+# if (num%2)==0:
+# print('Это число делится на 2')
+# elif (num%5)==0:
+# print('Это число делится на 5')
+# elif (num % 173) == 0:
+# print('Это число делится на 173')
+# elif (num%821)==0:
+# print('Это число делится на 821')
+# else:
+# print('Это число не делится на заданные в программе числа 2,5,173,821')
+
+/
